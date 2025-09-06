867b6e4f41392a6872dc21413e233a823c4a40ae
IanoNjuguna <inc3099@gmail.com>
2025-09-06 11:24:00 +0300

    Adding Connect Functionality between dApp and EVM wallet

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+import { createWalletClient, custom } from 'https://esm.sh/viem'
+
 const connectButton = document.getElementById("connectButton")
 
-connectButton.onclick = connect
+let walletClient = createWalletClient
 
-function connect() {
-	
+async function connect()
+{
+	if (typeof window.ethereum !== "undefined")
+	{
+		walletClient = createWalletClient({
+			transport: custom(window.ethereum)
+		})
+		await walletClient.requestAddresses()
+
+	}
+	else
+	{
+		connectButton.innerHTML = "Install Metamask!"
+	}
 }
 
+connectButton.onclick = connect
